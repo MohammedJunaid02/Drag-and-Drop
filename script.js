cdc46330c78f8b2326dcec977ffa23b87e76bb15
Mohammed Junaid <mohammedjunaidj08@gmail.com>
2023-10-30 00:09:53 +0530

    Added the logic for inserting the list elements at a specified position from one box to another and with preservation of order

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ let leftBox=document.getElementById("left");
 
 let selected=null;
 
-for(listItem of lists){
+
+// Just to drag and drop the list i.e without maintaining the insertion order at the specified position
+/*for(listItem of lists){
     listItem.addEventListener("dragstart",function(e){
         selected=e.target;
     });
@@ -30,7 +32,83 @@ for(listItem of lists){
             selected=null;
         });
 
+}*/
+
+// Just to drag and drop the list i.e with maintaining the insertion order at the specified position
+for (listItem of lists) {
+    listItem.addEventListener("dragstart", function (e) {
+        selected = e.target;
+    });
 }
+
+
+//for adding items from left to right
+rightBox.addEventListener("dragover", function (e) {
+    e.preventDefault();
+    const afterElement = getDragAfterElement(rightBox, e.clientY);
+    if (selected && afterElement) {
+        rightBox.insertBefore(selected, afterElement);
+    }
+});
+
+rightBox.addEventListener("drop", function (e) {
+    e.preventDefault();
+    if (selected) {
+        rightBox.appendChild(selected);
+        selected = null;
+    }
+});
+
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.list:not(.dragging)')];
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        if (offset < 0 && offset > closest.offset) {
+            return { offset, element: child };
+        } else {
+            return closest;
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element;
+
+}
+
+
+
+
+
+//for adding back items from right to left
+leftBox.addEventListener("dragover", function (e) {
+    e.preventDefault();
+    const afterElement = getDragAfterElement(leftBox, e.clientY);
+    if (selected && afterElement) {
+        leftBox.insertBefore(selected, afterElement);
+    }
+});
+
+leftBox.addEventListener("drop", function (e) {
+    e.preventDefault();
+    if (selected) {
+        leftBox.appendChild(selected);
+        selected = null;
+    }
+});
+
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.list:not(.dragging)')];
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        if (offset < 0 && offset > closest.offset) {
+            return { offset, element: child };
+        } else {
+            return closest;
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element;
+}
+
 
 
 
